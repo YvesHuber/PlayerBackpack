831939575ff3480c49f0ffa563ac8633d20408b0
YvesHuber <cpt.rino@gmail.com>
2021-06-07 16:11:33 +0200

    Readme

---
+++ Assets/Scripts/Player/ThirdPersonCameraControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraControl : MonoBehaviour
+{
+    float rotationSpeed = 1f;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+    bool looking = true;
+    public Transform Obstruction;
+    float zoomSpeed = 2f;
+    
+    void Start()
+    {
+        Obstruction = Target;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        CamControl();
+    }
+    
+
+    void CamControl()
+    {   
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -60, 60);
+
+        if (Input.GetMouseButton(1))
+        {
+            looking = false;
+            if (looking == false)
+            {
+            transform.LookAt(Player);
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);                
+            }
+        }
+        else
+        {
+            looking = true;
+        }
+        if (looking == true)
+        {
+        transform.LookAt(Target);
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # PlayerBackpack
+
+## About the Project
+
+This is a Unity Game Asset for a 3D First Person Game
+Simply steal the code from here or the Asset store (Not yet)
+and you are Done.
+With a right Click can you make new Game Object like:
+Items (Materials, Equapables, Consumables)
+Enemies
+Craftingstations
+Craftingrecepies
+Breakables (Trees, Stones etc.)
